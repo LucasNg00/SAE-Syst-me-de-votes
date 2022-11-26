daeab714eaebd5a0299583f18479d5f4eb9281a7
Lucas N'guyen <115957828+LucasNg00@users.noreply.github.com>
2022-11-26 11:56:49 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,74 +1,82 @@
+/*
+==========================
+Fait par Damien Stroppiana
+==========================
+*/
+
+#include <cstdlib>
 #include <iostream>
-
-
+#include <vector>
+#include <random>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 
-int main ()
-{
-    int gagnant ;
+void lireChoix(vector<string> & vChoix) {
+    while (true) {
+        string entree;
+        getline(cin, entree);
+        if (entree == "") break;
+        vChoix.push_back(entree);
+    }
+}
 
-    srand(1);
-    gagnant = rand() % 4;
-
-
-    int choixjeux,Counterstrike=0,StreetFighterII=0,CivilizationVI=0,MarioKart=0;
-
-    int j = 0 ;
-    while(j!= 112) {
-        choixjeux = rand() % 4;
-        switch(choixjeux)
-        {
-
-            case 1:
-                cout<<"\nUn electeur as voté pour Counter strike\n";
-                (Counterstrike)++;
-            break;
-            case 2:
-                cout<<"\nUn electeur as voté pour Street Fighter II\n";
-                (StreetFighterII)++;
-            break;
-            case 3:
-                cout<<"\nUn electeur as voté pour Civilization VI\n";
-                (CivilizationVI)++;
-            break;
-            case 4:
-                cout<<"\nUn electeur as voté pour Mario Kart\n";
-                (MarioKart)++;
-            break;
-
-            default:
-                cout<<"\nUn electeur as voté pour personne\n";
-
+void lireVotes(vector<vector<unsigned>> & vVote) {
+    while (true) {
+        string entreenom;
+        string Buf;
+        //vector <string> votant;
+        unsigned vote;
+        getline(cin, entreenom);
+        if (entreenom == "") break;
+        //votant.push_back(entreenom);
+        for (unsigned i = 0; i<size(vVote) ; ++i) {
+            cin >> vote;
+            vVote[i].push_back(vote);
         }
-        ++j;
+        getline(cin,Buf);
     }
-    switch(gagnant)
-    {
+}
 
-        case 1:
-            cout<<"\nLe gagnant est : Counter strike\n";
-            (Counterstrike)++;
-        break;
-        case 2:
-            cout<<"\nLe gagnant est : Street Fighter II\n";
-            (StreetFighterII)++;
-        break;
-        case 3:
-            cout<<"\nLe gagnant est : Civilization VI\n";
-            (CivilizationVI)++;
-        break;
-        case 4:
-            cout<<"\nLe gagnant est : Mario Kart\n";
-            (MarioKart)++;
-        break;
+void calcActions(vector<vector<unsigned>> & vVote, vector<unsigned> & tabScore) {
+    for (unsigned i=0 ; i<size(vVote);++i)
+        for (unsigned j=0; j<size(vVote[i]); ++j)
+            tabScore[i] += vVote[i][j];
+}
 
-        default:
-            cout<<"\nTu as voté pour personne, il n'y a pas de gagnant\n";
-
+void affResultats(vector<unsigned> & tabScore, vector<string> & vChoix) {
+    cout << "les résultats sont :" << endl;
+    for (unsigned i=0 ; i<size(tabScore); ++i){
+        cout << vChoix[i] << " : " << tabScore[i] << endl;
     }
+    unsigned iMax = 0, max = 0;
+    for (unsigned i=0 ; i<size(tabScore); ++i){
+        if(tabScore[i] > max){
+            iMax = i;
+            max = tabScore[i];
+        }
+    }
+    cout << "Le gagnant est : " << vChoix[iMax] << " avec " << tabScore[iMax];
+    cout << " voix." << endl;
+}
+
+int main()
+{
+    vector<string> vChoix;
+    lireChoix(vChoix);
+    if(size(vChoix) <= 1 || size(vChoix) > 10000) {
+        cerr << "Erreur : Merci de spécifier un nombre de choix entre 2 et 10000" ;
+        return 1;
+    }
+
+    vector <vector<unsigned>> mVote(size(vChoix));
+    vector<unsigned> tabScore(size(vChoix));
+    lireVotes(mVote);
+    calcActions(mVote, tabScore);
+    affResultats(tabScore, vChoix);
 
     return 0;
 }
-
